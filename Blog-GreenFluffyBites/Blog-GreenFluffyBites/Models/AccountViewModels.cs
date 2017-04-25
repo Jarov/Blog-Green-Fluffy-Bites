66e7840c4d2fe8b35c8c00a90407392acf7319d4
Yavor <qvalkov@gmail.com>
2017-04-25 14:44:55 +0300

    Edited Password requirements, and installed bootstrap theme!

--- Blog-GreenFluffyBites/Blog-GreenFluffyBites/Models/AccountViewModels.cs
+++ Blog-GreenFluffyBites/Blog-GreenFluffyBites/Models/AccountViewModels.cs
@@ -70,7 +70,7 @@ namespace Blog_GreenFluffyBites.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 4)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
@@ -89,7 +89,7 @@ namespace Blog_GreenFluffyBites.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 4)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
