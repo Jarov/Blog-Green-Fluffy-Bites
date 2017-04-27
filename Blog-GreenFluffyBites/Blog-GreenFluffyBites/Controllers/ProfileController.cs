6c555a085cbeae0b8ec98528642e59d17f1ed699
Yavor Valkov <qvalkov@gmail.com>
2017-04-27 17:55:52 +0300

    Made the like button to work and added profile page with the current user posts!

---
+++ Blog-GreenFluffyBites/Blog-GreenFluffyBites/Controllers/ProfileController.cs
@@ -0,0 +1,23 @@
+﻿using Blog_GreenFluffyBites.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Blog_GreenFluffyBites.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            using (var database = new BlogDBContext())
+            {
+                var authorId = database.Users.Where(u => u.UserName == this.User.Identity.Name).First().Id;
+
+                var articles = database.Articles.Include(a => a.Author).Where(a => a.Author.Id == authorId).ToList();
+
+                return View(articles);
+            }
+        }
+    }
+}
